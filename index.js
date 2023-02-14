430e5e4370de5bf7c9ecdfa306fa52886221c195
Jhonny Coliado <27758641+jhonnysc@users.noreply.github.com>
2023-02-14 10:57:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function runServerlessDeploy() {
   await exeq(
     `echo Running sls deploy with args ${ARGS}...`,
     `if [ ${process.env.AWS_ACCESS_KEY_ID} ] && [ ${process.env.AWS_SECRET_ACCESS_KEY} ]; then
-      sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} ${ARGS} --verbose
+      sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} --verbose
     fi`,
     `sls deploy ${ARGS} --verbose`
   )
